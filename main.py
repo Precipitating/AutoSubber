c73aaf96c2d21ad482ac2989c21850812e69f241
Eason Lee <eason-lee@hotmail.co.uk>
2024-12-26 03:36:08 +0000

    Changed using time to generate file names for mp3 to UID Successfully transcribed a selected audio file to text Updated gitignore to ignore mp3 files

--- main.py
+++ main.py
@@ -2,10 +2,9 @@ import os
 import customtkinter as ctk
 import moviepy
 from customtkinter import filedialog
-import whisper
 import time
-
-from whisper import transcribe
+import uuid
+from faster_whisper import WhisperModel
 
 
 class GUI(ctk.CTk):
@@ -14,6 +13,7 @@ class GUI(ctk.CTk):
         self.transcriber = Transcriber()
         self.geometry("350x400")
         self.title("AutoSubber")
+        self.resizable(False, False)
 
         inner_frame = ctk.CTkFrame(self)
         inner_frame.place(relx = 0.5, rely = 0.5, relwidth = 0.95, relheight=0.95,  anchor="center")
@@ -27,10 +27,12 @@ class GUI(ctk.CTk):
 
         # start button
         start_button = ctk.CTkButton(inner_frame, text="Start", command=self.start_button)
-        start_button.place(relx = 0.5, rely = 0.9,  anchor="center")
+        start_button.place(relx = 0.5, rely = 0.8,  anchor="center")
 
         # progress bar
-        progress_bar = ctk.CTkProgressBar(inner_frame)
+        self.progress_bar = ctk.CTkProgressBar(inner_frame, determinate_speed=10)
+        self.progress_bar.set(0)
+        self.progress_bar.place(relx = 0.5, rely = 0.9,  anchor="center")
 
 
     def browse_file(self):
@@ -47,31 +49,39 @@ class GUI(ctk.CTk):
     def start_button(self):
         # convert to mp3
         self.transcriber.extract_audio(self.selected_file.get())
+        self.progress_bar.step()
 
         # transcribe via Whisper
         self.transcriber.transcribe()
+        self.progress_bar.step()
 
 
 
 class Transcriber:
     def __init__(self):
-        self.model = whisper.load_model('large')
-        self.file_name = str(time.time())
+        self.model = WhisperModel("large-v3")
+        self.file_name = f"{uuid.uuid4().hex}"
         self.transcription_result = ""
+        self.output_dir = "output/"
 
     def extract_audio(self, path):
         # load video
         clip = moviepy.VideoFileClip(path)
 
         # convert to mp3 using timestamp as file name
-        clip.audio.write_audiofile(f'{self.file_name}.mp3')
+        clip.audio.write_audiofile(f'{self.output_dir}{self.file_name}.mp3')
 
     def transcribe(self):
-        self.transcription_result = self.model.transcribe(
-            self.file_name,
-            task = 'translate',
-            word_timestamps= True
-        )
+        segments, info = self.model.transcribe(f'{self.output_dir}{self.file_name}.mp3')
+        segments = list(segments)
+
+        for seg in segments:
+            print("[%.2fs -> %.2fs] %s" % (seg.start, seg.end, seg.text))
+        return info.language, segments
+
+
+   # def apply_caption_to_video:
+
 
 
 
